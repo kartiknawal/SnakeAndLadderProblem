167dab678177ebb625feee6321d52ce467e21cc4
kartiknawal <kartiknawal.7@gmail.com>
2020-09-29 15:14:11 +0530

    [kartiknawal] add initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnakeAndLadderProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Snake and Ladder Simulator");
+        }
+    }
+}
