14b1c41ad6075de971f6bbef690624d7d60a06b5
kartiknawal <kartiknawal.7@gmail.com>
2020-09-29 15:46:54 +0530

    [kartiknawal] add dice roll

--- Program.cs
+++ Program.cs
@@ -10,7 +10,17 @@ namespace SnakeAndLadderProblem
             int score;
             score = 0;
             Console.WriteLine("Initial score : " + score);
+
+            score = DiceRoll();
+            Console.WriteLine("DiceRoll " + score);
+
             return;
+        }
+        static int DiceRoll()
+        {
+            Random random = new Random();
+            int diceNumber = random.Next(1, 7);
+            return diceNumber;
         }
     }
 }
